f4e8c35ca0b66899b850e842106f93dc22f4b9ae
Marcuslopez <marcosramos656@gmail.com>
2026-03-21 20:53:56 -0500

    fix: puerto dinámico para azure

--- src/api/src/main.ts
+++ src/api/src/main.ts
@@ -19,7 +19,8 @@ async function bootstrap() {
   SwaggerModule.setup('swagger', app, document);
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  //await app.listen(port);
+  await app.listen(process.env.PORT || port);
 
   
   
