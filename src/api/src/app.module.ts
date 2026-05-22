8c0c472358213dd9b7627e9ab3bbba11084f83e4
Marcuslopez <marcosramos656@gmail.com>
2026-05-21 21:19:41 -0500

    modify res auth

--- src/api/src/app.module.ts
+++ src/api/src/app.module.ts
@@ -7,6 +7,7 @@ import { HealthModule } from './health/health.module';
 import { FilesModule } from './files/files.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
