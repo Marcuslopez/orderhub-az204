3caab226c929bfd65253a2090a6fc8658c501527
Marcuslopez <marcosramos656@gmail.com>
2026-06-05 18:50:35 -0500

    elimino roles.decorator.ts.spec.ts

--- src/api/src/auth/roles.decorator.ts.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RolesDecoratorTs } from './roles.decorator.js';
-
-describe('RolesDecoratorTs', () => {
-  it('should be defined', () => {
-    expect(new RolesDecoratorTs()).toBeDefined();
-  });
-});
